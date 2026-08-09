7724d8faf889cf275803b6091430106ad22aae22
Antonio González Gómez <antoniogg696@gmail.com>
2026-08-09 18:54:51 +0200

    Fixed collectibles not being removed

--- collectible/collectible_manager.gd
+++ collectible/collectible_manager.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
-@export var ammount := 5
+@export var ammount := 8
 
 var collectibles: Array[Node] = []
 
 func _ready() -> void:
 	collectibles = find_children("*", "Area3D")
-	#Deletes random collectibles from the map to only have the set ammount
 	collectibles.shuffle()
-	collectibles.resize(ammount)
+	#Deletes random collectibles from the map to only have the set ammount
+	var excess = collectibles.slice(ammount)
+	for c in excess:
+		c.queue_free()
+
 	GlobalAutoload.difficulty_inc = 1.0 / ammount
